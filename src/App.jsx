aefa861ff2a6e55974d554251c46e50f84fadeda
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-01 14:19:06 +0545

    3.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Shop from './pages/Shop'
 function App() {
  
 
@@ -8,6 +9,7 @@ function App() {
     <>
   <Routes>
     <Route path='/' element={<Home/>} />
+    <Route path='/shop' element={<Shop/>} />
   </Routes>
      </>
   )
