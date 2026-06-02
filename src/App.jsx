715da16d651a57274583ae8ea857209579ea0743
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-02 14:34:03 +0545

    3.3

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Shop from './pages/Shop'
-import Product from './pages/Product'
+import Product from './pages/Product';
+import Login from './pages/Login'
 function App() {
  
 
@@ -12,6 +13,7 @@ function App() {
     <Route path='/' element={<Home/>} />
     <Route path='/shop' element={<Shop/>} />
     <Route path='/product' element={<Product />} />
+    <Route path='/login' element={<Login />} />
   </Routes>
      </>
   )
