c76f3560f60b1e7f6d034f6b8b100fd1515a2170
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-02 14:48:32 +0545

    3.4

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
     <Route path='/shop' element={<Shop/>} />
     <Route path='/product' element={<Product />} />
     <Route path='/login' element={<Login />} />
+    <Route path='/product' element={<Product />} />
   </Routes>
      </>
   )
