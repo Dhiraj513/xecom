d04df7e14cb06f24dc82ee38c828864c03718b44
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-07 15:45:41 +0545

    4.9

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -3,7 +3,12 @@ const Header = () => {
   return (
     
     <header>
-      
+      <div className="bg-green-200">
+        <div className="text-sm max-w-360 mx-auto py-1 lg:px-8 px-5">
+        Free Delivery on your first order!
+        </div>
+
+      </div>
     </header>
   )
 }
