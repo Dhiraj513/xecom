70abbce901c355c3625c76e0d29419f0f8295b6b
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-12 00:28:46 +0545

    7.0

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
             <div className="grid grid-cols-2">
         {/*left box */}
         <div className="space-y-7">
-        <h2 className="lg:text-5xl text-3xl text-gray-800 font-bold leading-14">Discover Your Style With Our Collection</h2>
+        <h2 className="lg:text-5xl text-3xl text-gray-700 font-bold leading-14">Discover Your Style With Our Collection</h2>
         </div>
             </div>
 
