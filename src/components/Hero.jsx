edcaf7008d2fe13e488621ecb9629d220d83fd83
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-12 00:20:13 +0545

    6.7

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="bg-slate-50">
+        <div className="max-w-360 mx-auto lg:px-8 px-5">
+            <div></div>
+
+        </div>
+
+    </div>
   )
 }
 
