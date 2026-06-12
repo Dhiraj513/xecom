2bedbe448a1e67567f6fe313bff6ca5c113c8b5a
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-13 00:45:23 +0545

    7.5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="bg-slate-50">
         <div className="max-w-360 mx-auto lg:px-8 px-5">
-            <div className="grid grid-cols-2">
+            <div className="grid grid-cols-2 items-center">
         {/*left box */}
         <div className="space-y-7 py-10">
         <h2 className="lg:text-5xl text-3xl text-gray-700 font-bold leading-14">Discover Your Style With Our Collection</h2>
@@ -13,7 +13,7 @@ const Hero = () => {
         </div>
             
         {/*right box */       }
-        <div>
+        <div className="rlative">
         <img className="rounded-2xl object-cover h-75 sm:h-125 relative w-full" src={Banner} alt="" />
         </div>
         </div>
