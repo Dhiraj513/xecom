33869c00e2c72c0cda26128bda9da3d62008443f
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-13 00:34:52 +0545

    7.2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 const Hero = () => {
   return (
@@ -5,9 +6,10 @@ const Hero = () => {
         <div className="max-w-360 mx-auto lg:px-8 px-5">
             <div className="grid grid-cols-2">
         {/*left box */}
-        <div className="space-y-7">
+        <div className="space-y-7 py-10">
         <h2 className="lg:text-5xl text-3xl text-gray-700 font-bold leading-14">Discover Your Style With Our Collection</h2>
-        <p>Explore our curated selection of premium products designed to elecate your lifestyle.Quality meets style in every piece.</p>
+        <p className="text-lg text-gray-800">Explore our curated selection of premium products designed to elecate your lifestyle.Quality meets style in every piece.</p>
+         <Link className="bg-green-400 px-8 py-3  text-md rounded-md text-gray-800">Shop Now</Link>
         </div>
             </div>
 
