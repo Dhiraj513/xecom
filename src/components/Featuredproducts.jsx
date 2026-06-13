c2eb8206e99b82f063a3f86142872f9500854014
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-14 00:27:25 +0545

    8.0

--- src/components/Featuredproducts.jsx
+++ src/components/Featuredproducts.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+
 
 const Featuredproducts = () => {
   return (
-    <div>Featuredproducts</div>
+    <div>
+        <h2 className="text-green-400 text-4xl font-medium pb-10">
+            Featured
+            <span className="text-gray-700"> Products</span>
+        </h2>
+    </div>
   )
 }
 
