a44f476b4365ba2cf26696f9222813a077ed2631
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-14 00:21:22 +0545

    7.6

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,9 @@ const Hero = () => {
   return (
     <div className="bg-slate-50">
         <div className="max-w-360 mx-auto lg:px-8 px-5">
-            <div className="grid grid-cols-2 items-center">
+            <div className="grid grid-cols-2 items-center py-20">
         {/*left box */}
-        <div className="space-y-7 py-10">
+        <div className="space-y-7 py-10 pr-20">
         <h2 className="lg:text-5xl text-3xl text-gray-700 font-bold leading-14">Discover Your Style With Our Collection</h2>
         <p className="text-lg text-gray-800">Explore our curated selection of premium products designed to elecate your lifestyle.Quality meets style in every piece.</p>
          <Link className="bg-green-400 px-8 py-3  text-md rounded-md text-gray-800">Shop Now</Link>
