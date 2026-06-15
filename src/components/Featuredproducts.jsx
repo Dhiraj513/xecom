210a6b4be33418540ad8a05d3ae63cc2d5ef1343
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-15 22:43:59 +0545

    8.4

--- src/components/Featuredproducts.jsx
+++ src/components/Featuredproducts.jsx
@@ -2,7 +2,7 @@
 
 const Featuredproducts = () => {
   return (
-    <div className="max-w-360 mx-auto lg:px-8 px-5">
+    <div className="max-w-360 mx-auto lg:px-8 px-5 py-10">
         <h2 className="text-green-400 text-4xl font-medium pb-10">
             Featured
             <span className="text-gray-700"> Products</span>
