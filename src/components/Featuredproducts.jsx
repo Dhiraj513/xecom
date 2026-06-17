79e4a80e337944f364c042940ff22bda1740a6c8
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-06-18 00:48:15 +0545

    9.8

--- src/components/Featuredproducts.jsx
+++ src/components/Featuredproducts.jsx
@@ -9,15 +9,15 @@ const Featuredproducts = () => {
             <span className="text-gray-700"> Products</span>
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5">
-            <div className="space-y-5 ">
-            <div className="bg-slate-100 overflow-hidden rounded-lg">
+            <div  >
+            <div className="bg-slate-100 overflow-hidden rounded-lg ">
                 <img
                  className="object-cover h-95 duration-500 ease-in-out transition-transform hover:scale-110" 
                  src={ProductImage1} alt="" />
             </div>
             <h3 className="text-lg text-gray-800 font-semibold">Dummy Product</h3>
-            <p className="text-base text-gray-700">NRS 1000
-                <
+            <p className="text-base text-gray-700">NRS 1000 {" "}
+                <span className="line-through text-gray-700">   NRS 1200</span>
             </p>
 
              <div className="bg-slate-100 overflow-hidden rounded-lg">
