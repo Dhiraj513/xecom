8861190ff91869f519760cf2e309691e149c4cba
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-07-07 23:55:20 +0545

    17.3

--- src/components/LatestProduct.jsx
+++ src/components/LatestProduct.jsx
@@ -1,5 +1,42 @@
 
 const LatestProduct = () => {
+   const products = [
+          {
+              id: 1,
+              name: "Dummy Product 1",
+              image: ProductImage1,
+              price: 1000,
+              crossPrice: 1100
+          },
+           {
+              id: 2,
+              name: "Dummy Product 2",
+              price: 1100,
+              image: ProductImage2,
+              crossPrice: 1200
+          },
+           {
+              id: 3,
+              name: "Dummy Product 3",
+              price: 1000,
+              image: ProductImage1,
+              crossPrice: 1200
+          },
+           {
+              id: 4,
+              name: "Dummy Product 4",
+              price: 1500,
+              image: ProductImage2,
+              crossPrice: 1700
+          },
+           {
+              id: 5,
+              name: "Dummy Product 5",
+              price: 1800,
+              image: ProductImage1,
+              crossPrice: 2000
+          }
+      ]
   return (
     
   )
