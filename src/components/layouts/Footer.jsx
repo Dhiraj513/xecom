e1ac282168ca55397ddc727c3fe404245cf5cfec
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-07-11 20:32:05 +0545

    18.4

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,8 +1,11 @@
 
 const Footer = () => {
   return (
-    <div className="max-w-360 mx-auto"
-    ></div>
+    <div className="max-w-360 mx-auto bg-green-300 lg:px-8 px-5">
+      <div className="">
+
+      </div>
+    </div>
   )
 }
 
