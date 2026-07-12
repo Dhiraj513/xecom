94b5314630af19c1c96f36e26c1b02ec7f7e0257
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-07-12 22:28:20 +0545

    18.8

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <div className="max-w-360 mx-auto lg:px-8 px-5">
       <div className="grid grid-cols-1 lg:grid-cols-3 bg-green-300">
-      <div></div>
+      <div> all rights reserved</div>
       </div>
     </div>
   )
