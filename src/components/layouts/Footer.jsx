f375dcac9c2df8f7dab05f102faa57f72c73eedb
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-07-14 01:46:33 +0545

    19.8

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <div className="max-w-360 mx-auto lg:px-8 px-5">
       <div className="grid grid-cols-1 lg:grid-cols-3 bg-green-300 rounded-lg p-10">
-      <div className=""> 
+      <div className="lg:pr-20"> 
         <Link>
         <span className="text-3xl text-white font bold" >X</span>
         <span className="text-3xl text-slate-800 font-bold">ECOM</span>
