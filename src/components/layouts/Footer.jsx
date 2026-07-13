d73665b7af644bfa1de64366190f8fbe05042af9
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-07-13 16:31:25 +0545

    19.1

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,8 +2,12 @@
 const Footer = () => {
   return (
     <div className="max-w-360 mx-auto lg:px-8 px-5">
-      <div className="grid grid-cols-1 lg:grid-cols-3 bg-green-300 rounded-lg">
-      <div> all rights reserved</div>
+      <div className="grid grid-cols-1 lg:grid-cols-3 bg-green-300 rounded-lg p-10">
+      <div> 
+        <Link>
+        <span className="text-3xl text-white font bold" >X</span>
+          </Link>
+      </div>
       <div>made with intention</div>
       </div>
     </div>
