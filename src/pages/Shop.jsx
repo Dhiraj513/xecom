09402f376f555f65f7d974febecd2a779cf5ae62
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-07-21 00:44:32 +0545

    22.6

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,13 +1,43 @@
-import Header from "../components/layouts/Header"
-import Footer from "../components/layouts/Footer"
+import { Link } from "react-router-dom"
+import Layout from "../components/layouts/Layout"
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Shop = () => {
   return (
     <>
-    <Header/>
+    <Layout>
+    {/* breadcrum */}
+    <div className="max-w-360 lg:px-8 px-5 mx-auto py-5">
+      <div className="flex gap-x-1 items-center text-sm">
+        <Link>Home</Link>
+        <MdOutlineKeyboardArrowRight />
+        <Link className="font-bold">Shop</Link>
+      </div>
+    </div>
+    {/*Title */}
+     <div className="max-w-360 lg:px-8 px-5 mx-auto">
+      <div className="flex text-4xl text-green-400">
+        Shop
+      </div>
+    </div>
 
-    
-    <Footer/>
+    {/*main Container */}
+    <div className="max-w-360 lg:px-8 px-5 mx-auto">
+      <div className="grid grid-cols-12">
+
+    {/*sidebar */}
+    <div className="col-span-3">
+
+    </div>
+
+    {/* producs */}
+    <div className="col-span-9">
+
+    </div>
+      </div>
+    </div>
+
+    </Layout>
     </>
   )
 }
