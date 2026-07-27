758657d3991ba875200fbdd03b358b3dfa5ac1f6
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-07-27 22:26:17 +0545

    24.8

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -36,6 +36,16 @@ const Shop = () => {
 <input type="checkbox" className="w-4 h-4 border border-gray-400" />
 <span> Topwear</span>
 </div>
+
+<div className="flex items-center space-x-2">
+<input type="checkbox" className="w-4 h-4 border border-gray-400" />
+<span> Topwear</span>
+</div>
+<div className="flex items-center space-x-2">
+<input type="checkbox" className="w-4 h-4 border border-gray-400" />
+<span> Topwear</span>
+</div>
+
     </div>
     {/* producs */}
     <div className="col-span-9 border">
