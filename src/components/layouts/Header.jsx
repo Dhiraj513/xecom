26f7c4d0348fc7edd4327337917a444dc9e97953
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-08-08 17:35:53 +0545

    29.3

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -3,7 +3,7 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 const Header = () => {
   return (
     
-    <header>
+    <header className="shadow">
       <div className="bg-green-200">
         <div className="text-sm max-w-360 mx-auto py-1 lg:px-8 px-5">
         Free Delivery on your first order!
