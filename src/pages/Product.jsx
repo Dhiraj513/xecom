7e79e6f208de619fd4c8dfaba25a3f96dd5016cb
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-08-09 22:36:20 +0545

    30.1

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,10 +1,19 @@
+import { Link } from "react-router-dom"
 import Layout from "../components/layouts/Layout"
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 
 const product = () => {
   return (
    <Layout>
-    
+     {/* breadcrum */}
+    <div className="max-w-360 lg:px-8 px-5 mx-auto py-5">
+      <div className="flex gap-x-1 items-center text-sm">
+        <Link>Home</Link>
+        <MdOutlineKeyboardArrowRight />
+        <Link className="font-bold">Shop</Link>
+      </div>
+    </div>
    </Layout>
   )
 }
