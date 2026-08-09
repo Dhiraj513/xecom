01a9579cb757b0d2c179ac5be82f22c7e033105a
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-08-09 22:37:58 +0545

    30.2

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -9,9 +9,9 @@ const product = () => {
      {/* breadcrum */}
     <div className="max-w-360 lg:px-8 px-5 mx-auto py-5">
       <div className="flex gap-x-1 items-center text-sm">
-        <Link>Home</Link>
+        <Link>Shop</Link>
         <MdOutlineKeyboardArrowRight />
-        <Link className="font-bold">Shop</Link>
+        <Link className="font-bold">Product for men</Link>
       </div>
     </div>
    </Layout>
