e9ea446bb90a5fd3f215540907b13341b73d7d78
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-08-10 23:12:22 +0545

    30.8

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom"
 import Layout from "../components/layouts/Layout"
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import ProductImg1 from "../assets/product1.png";
+import ProductImg2 from "../assets/product2.jpg";
+import ProductImg3 from "../assets/product3.png";
 
 
-const product = () => {
+const Product = () => {
+
+    const gallery = [
+      {
+        id:1,
+        url:ProductImg1
+      },
+      {
+        id:1,
+        url:ProductImg2
+      },
+      {
+        id:1,
+        url:ProductImg3
+      }
+    ]
   return (
    <Layout>
      {/* breadcrum */}
@@ -18,7 +36,23 @@ const product = () => {
      <div className="max-w-360 lg:px-8 px-5 mx-auto py-5">
       <div className="grid grid-cols-12 gap-5 ">
         <div className="lg:col-span-6 col-span-12 border">
-jgkh
+      {/*Gallery */}
+      <div className="grid grid-cols-12">
+        <div className="col-span-10">
+          {/*Main Images */}
+        </div>
+        <div className="col-span-2">
+          {/* Small Images */}
+          {
+            gallery && gallery.map(item => {
+              return (
+                <img src="" alt="" />
+              )
+            })
+          }
+        </div>
+
+      </div>
         </div>
         <div className="lg:col-span-6 col-span-12 border">
 jghf
@@ -30,4 +64,4 @@ jghf
   )
 }
 
-export default product
+export default Product
