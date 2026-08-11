b44f2932c7d5271c59690ae0ba75d71d2edd76af
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-08-11 23:06:32 +0545

    31.1

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -46,7 +46,7 @@ const Product = () => {
           {
             gallery && gallery.map(item => {
               return (
-                <img src="" alt="" />
+                <img src=" " alt="" />
               )
             })
           }
