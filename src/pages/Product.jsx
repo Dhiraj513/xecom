554f25f78121898f1816c2442ef01c60261e656e
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-08-15 22:24:17 +0545

    32.6

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -48,7 +48,7 @@ const Product = () => {
       <div className="grid grid-cols-12">
         <div className="col-span-10">
           {/*Main Images */}
-          <img src={mainImage?.url} alt="" />
+          <img src={mainImage?.url} className="w-full" alt="" />
                  </div>
         <div className="col-span-2">
           {/* Small Images */}
