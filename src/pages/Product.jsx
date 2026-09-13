a8b42af516f7f07b8cf8c63ed8af731c5340289e
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-09-13 23:07:18 +0545

    37.6

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -71,7 +71,7 @@ const Product = () => {
         </div>
         <div className="lg:col-span-7 col-span-12 ">
           {/*Product info */}
-          <div className="font-bold text-2xl text-gray-800">Good product for men </div>
+          <h2 className="font-bold text-2xl text-gray-800">Good product for men </h2>
 
           {/*price */}
           <div className="font-bold text-lg">
