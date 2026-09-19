f77dd787e4465195d123ca6727323371c72f95e0
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-09-19 23:38:27 +0545

    38.5

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -89,7 +89,7 @@ const Product = () => {
           <div className="font-bold text-lg mt-3">Select size</div>
 
           <div className="flex gap-x-2"></div>
-             <div className="BG bg-gray-300 rounded-md w-10 h-10 text-center">S</div>
+             <div className="BG bg-gray-300 rounded-md w-10 h-10 text-center justofy-center">S</div>
              <div>M</div>
              <div>L</div>
              <div>XL</div>
