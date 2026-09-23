447120c5d600804e14f5904990d2e78bd11201ee
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-09-23 23:25:32 +0545

    40.0

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -88,11 +88,12 @@ const Product = () => {
           {/*sizes */}
           <div className="font-bold text-lg mt-3">Select size</div>
 
-          <div className="flex gap-x-2"></div>
+          <div className=" flex gap-x-2">
              <div className="bg-gray-100 hover:bg-gray-800 hover:text-white cursor-pointer rounded-md w-10 h-10 text-center flex justify-center items-center">S</div>
              <div className="bg-gray-100 hover:bg-gray-800 hover:text-white cursor-pointer rounded-md w-10 h-10 text-center flex justify-center items-center">M</div>
              <div className="bg-gray-100 hover:bg-gray-800 hover:text-white cursor-pointer rounded-md w-10 h-10 text-center flex justify-center items-center">L</div>
              <div className="bg-gray-100 hover:bg-gray-800 hover:text-white cursor-pointer rounded-md w-10 h-10 text-center flex justify-center items-center">XL</div>
+             </div>
         </div>
        
       </div>
