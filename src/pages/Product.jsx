80d299a5c1f550f7d1f319b71a8cb37aa848487f
Dhiraj513 <121927685+Dhiraj513@users.noreply.github.com>
2026-09-24 16:06:55 +0545

    40.5

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -95,7 +95,7 @@ const Product = () => {
              <div className="bg-gray-100 hover:bg-gray-800 hover:text-white cursor-pointer rounded-md w-10 h-10 text-center flex justify-center items-center">XL</div>
              </div>
             
-            <div className="mt-10">
+            <div className="mt-5 border-b border-gray-200 pb-10">
               <Link className="bg-green-400 px-5 py-3  text-md rounded-md text-gray-800">
               Add to cart
               </Link>
